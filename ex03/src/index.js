98fb86957fa34a671b9cd4ebcab0fd710188b920
aharis <hare.arapovic@gmail.com>
2021-03-11 17:55:05 +0100

    dodao index.js file decrement

---
+++ ex03/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+
+    var myRes = 16;
+    
+    // Only change code below this line
+    
+    if (myRes > 14){
+    
+        myRes --;
+    }
+    // Only change code above this line
+        return myRes;
+    }
+    
+        console.log(main());
+    
+        module.exports = main;
