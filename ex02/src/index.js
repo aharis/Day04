fc24afdb4ad52d98fa147dc55d2d464a13ed169c
aharis <hare.arapovic@gmail.com>
2021-03-11 14:44:36 +0100

    dodao index.js file with incrament

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+
+var myRes = 75;
+
+// Only change code below this line
+
+if (myRes < 77){
+
+    myRes ++;
+}
+// Only change code above this line
+    return myRes;
+}
+
+    console.log(main());
+
+    module.exports = main;
