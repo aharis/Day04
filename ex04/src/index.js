40c013d9e947c3b6026f88ce7cf4ecb266d3cb24
aharis <hare.arapovic@gmail.com>
2021-03-11 19:10:59 +0100

    doao index.js file with decimal velues

---
+++ ex04/src/index.js
@@ -0,0 +1,21 @@
+function main(){
+
+    //Only change code below this line
+
+    var myDecimal = 84;
+    var myDecimalE = 254e-5;
+    myDecimal = myDecimal / 10;
+
+    // Only change code above this line
+    return {
+
+        myDecimal,
+        myDecimalE
+    };
+
+    
+}
+
+    console.log(main());
+
+    module.exports = main;
