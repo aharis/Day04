ea6343a3e7dfb39d0582c3467b29c68091f9665d
aharis <hare.arapovic@gmail.com>
2021-03-11 20:54:39 +0100

    dodao index.js file addition and subtraction

---
+++ ex08/src/index.js
@@ -0,0 +1,26 @@
+function main(){
+
+    //Only change code below this line
+     var myX = 12;
+     myX += 3;
+
+     myY = 19;
+     myY += 17;
+
+     var myZ = 24;
+     myZ -= 20;
+   
+ 
+    // Only change code above this line
+    
+    return {
+        myX,
+        myY,
+        myZ
+
+    };
+}
+
+        console.log(main());
+      
+        module.export = main;
